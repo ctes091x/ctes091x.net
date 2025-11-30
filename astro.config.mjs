5329dd55779c5639dd2105c98181f3c13ad0bb2a
ctes091x <ctes091x@ctes091x.net>
2025-11-30 19:21:19 +0900

    add files for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ctes091x.net',
+  base: '/ctes091x.net',
+});
