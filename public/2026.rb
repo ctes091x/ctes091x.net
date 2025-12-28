dd8aa0754b2729695a3b8302ad0904931494533b
ctes091x <ctes091x@ctes091x.net>
2025-12-28 14:37:31 +0900

    Create 2026.rb

---
+++ public/2026.rb
@@ -0,0 +1,15 @@
+eval$s=%q(eval(%w(
+
+ s=%    (ev    al$     s= 
+%   q  (   #  {   $   s   
+    }  )  );      p  u    
+   t   s ( s     )   #### 
+  #    ##  #    #    #   #
+ #     #   #   #     #   #
+#####   ###   #####   ### 
+
+# 本年もよろしくお願いいたします。
+# HP: https://ctes091x.net
+# Email: ctes091x@ctes091x.net
+
+)*""))
